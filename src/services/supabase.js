dbec6909002d0686d534131b56c1fe128ea4f649
Karimatif33 <karimatif33@gmail.com>
2024-02-16 19:02:09 +0200

    finshing th supbase seting and link it to the project

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://izoxhawhicdbwalmzfyn.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6b3hoYXdoaWNkYndhbG16ZnluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwOTc2NTAsImV4cCI6MjAyMzY3MzY1MH0.sFbxxzcluCyKKA8iJxFGf9ruzoLTnl7K-ngsQ9etZW0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
