ca679bfe76523b2b338943526c3c551ea2d27682
Karimatif33 <karimatif33@gmail.com>
2024-02-24 07:35:55 +0200

    working on filters

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,18 +2,19 @@ import Heading from "../../ui/Heading";
 import Row from "../../ui/Row";
 import CabinTable from "../../features/cabins/CabinTable";
 import AddCabin from "../../src/pages/AddCabin";
+import CabinTableOperations from "../../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
     <>
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>Filter/Sort</p>
-    </Row>
-    <Row>
-      <CabinTable/>
-     <AddCabin />
-    </Row>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <CabinTableOperations />
+      </Row>
+      <Row>
+        <CabinTable />
+        <AddCabin />
+      </Row>
     </>
   );
 }
