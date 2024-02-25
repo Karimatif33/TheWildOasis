e684ec16092cbe2b83b4aa2e102b3c2ddea5175e
Karimatif33 <karimatif33@gmail.com>
2024-02-25 06:50:07 +0200

    finishing filters and sort

--- features/cabins/CabinTableOperations.jsx
+++ features/cabins/CabinTableOperations.jsx
@@ -1,10 +1,28 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
     <TableOperations>
-      <Filter />
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "Nodiscount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by price (Low First)" },
+          { value: "regularPrice-desc", label: "Sort by price (High First)" },
+          { value: "maxCapacity-asc", label: "Sort by Capacity (Low First)" },
+          { value: "maxCapacity-desc", label: "Sort by Capacity (High First)" },
+        ]}
+      />
     </TableOperations>
   );
 }
