177a317201e018e4cd2908107216713911ef6891
Karimatif33 <karimatif33@gmail.com>
2024-02-26 14:40:12 +0200

    finishing filters and sort in bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,9 +1,16 @@
 import { getToday } from "../../utils/helpers";
 import supabase from "./supabase";
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select("*, cabins(name), guests(fullName,email)");
+  if (filter) query = query.eq(filter.field, filter.value);
+  
+  if(sortBy) 
+  query = query.order(sortBy.field,{ascending: sortBy.direction === 'asc',})
+  
+  
+  const { data, error } = await query;
   if (error) {
     console.error(error);
     throw new Error(" Booking coude not be loaded ");
